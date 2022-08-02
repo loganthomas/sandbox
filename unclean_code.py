6f00579ec76d8998e538d056732073f57dc8745a
loganthomas <logan.thomas005@gmail.com>
2022-08-02 17:52:07 -0500

    TEST: add uncleaned code

---
+++ unclean_code.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+
+def func1(x,y):
+    return x+y
+
+def func2(x,y):
+    return x*y
+
+if __name__ == '__main__':
+    print(func1(4,5))
+    print(func2(3,5))
