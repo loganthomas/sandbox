f907c67b5c5fe57ed78b4114f343d17befb31f4a
loganthomas <logan.thomas005@gmail.com>
2022-08-01 20:49:31 -0500

    Revert "[pre-commit.ci] auto fixes from pre-commit.com hooks"
    This reverts commit cce0b84ae19019a82106393f76288d88b207922e.

--- code.py
+++ code.py
@@ -1,16 +1,15 @@
+import numpy as np
+import pandas as pd
+
 import datetime
 from collections import namedtuple
 from pathlib import Path
-
-import numpy as np
-import pandas as pd
-import tensorflow as tf
 from sklearn import datasets
+import tensorflow as tf
 
 print("hello world")
-print("hello universe")
-z = 5 + 3
-
+print('hello universe')
+z = 5+3
 
 print('hi logan')
 def dummy_func(x):
