6de440b7c4f7acc797378e862a08fcc9e9ebaf00
loganthomas <logan.thomas005@gmail.com>
2022-08-02 23:10:12 -0500

    TEST: dirty up code for black and isort

--- code.py
+++ code.py
@@ -9,11 +9,9 @@ from sklearn import datasets
 
 print("hello world")
 print("hello universe")
-z = 5 + 3
+z = 5+3
 print("hi logan")
 print("hello universe")
-
-
 def dummy_func(x):
     return x + 2
 
--- code2.py
+++ code2.py
@@ -1,10 +1,7 @@
-from collections import Counter
-
-import seaborn as sns
 from matplotlib import pyplot as plt
+from collections import Counter
+import seaborn as sns
 
 print("hi")
-
-
 def dummy(s="logan"):
     print(f"hi {s}")
